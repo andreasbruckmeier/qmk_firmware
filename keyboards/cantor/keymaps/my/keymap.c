d042cfc790885d5f39cf0fd3d3c47987e7dc6a24
Autopwn <autopwn@ubi17.de>
2022-06-20 10:42:38 +0200

    Switched from left alt back to altgr for umlauts

--- keyboards/cantor/keymaps/my/keymap.c
+++ keyboards/cantor/keymaps/my/keymap.c
@@ -6,7 +6,7 @@
 
 #define SPC_SHL  LSFT_T(KC_SPC)
 #define SPC_SH   RSFT_T(KC_SPC)
-#define ENT_LCA  LCA_T(KC_ENT)
+#define ENT_RALT RALT_T(KC_ENT)
 #define BSP_CT   LCTL_T(KC_BSPC)
 
 #define RAISE    MO(_RAISE)
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC  , KC_Q , KC_W , KC_F , KC_P  , KC_B ,                     KC_J   , KC_L , KC_U    , KC_Y   , KC_SCLN , KC_MINS ,
         KC_TAB  , KC_A , KC_R , KC_S , KC_T  , KC_G ,                     KC_M   , KC_N , KC_E    , KC_I   , KC_O    , KC_QUOT ,
         KC_LALT , KC_Z , KC_X , KC_C , KC_D  , KC_V ,                     KC_K   , KC_H , KC_COMM , KC_DOT , KC_SLSH , KC_PIPE ,
-                                       RAISE , SPC_SHL, BSP_CT , ENT_LCA , SPC_SH , LOWER
+                                       RAISE , SPC_SHL, BSP_CT , ENT_RALT , SPC_SH , LOWER
     ),
      /* RAISE
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
