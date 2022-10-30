c712eaab9b94797bdff24ac16429a10ca2409152
Autopwn <autopwn@ubi17.de>
2022-10-30 14:19:02 +0100

    Change Tab to LT(Lower, Tab)

--- keyboards/cantor/keymaps/my/keymap.c
+++ keyboards/cantor/keymaps/my/keymap.c
@@ -8,6 +8,7 @@
 #define SPC_SH   RSFT_T(KC_SPC)
 #define ENT_RALT RALT_T(KC_ENT)
 #define BSP_CT   LCTL_T(KC_BSPC)
+#define LOW_TAB  LT(LOWER, KC_TAB) 
 
 #define RAISE    MO(_RAISE)
 #define LOWER    MO(_LOWER)
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [_COLEMAK_DH] = LAYOUT_split_3x6_3(
         KC_ESC  , KC_Q , KC_W , KC_F , KC_P  , KC_B ,                     KC_J   , KC_L , KC_U    , KC_Y   , KC_SCLN , KC_MINS ,
-        KC_TAB  , KC_A , KC_R , KC_S , KC_T  , KC_G ,                     KC_M   , KC_N , KC_E    , KC_I   , KC_O    , KC_QUOT ,
+        LOW_TAB  , KC_A , KC_R , KC_S , KC_T  , KC_G ,                     KC_M   , KC_N , KC_E    , KC_I   , KC_O    , KC_QUOT ,
         KC_LALT , KC_Z , KC_X , KC_C , KC_D  , KC_V ,                     KC_K   , KC_H , KC_COMM , KC_DOT , KC_SLSH , KC_PIPE ,
                                        RAISE , SPC_SHL, BSP_CT , ENT_RALT , SPC_SH , LOWER
     ),
